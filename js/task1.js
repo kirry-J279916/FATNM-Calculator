9bedbf5d085743d8d1f91b741b8c65f6e9e82f8f
j279916 <j279916@tafe.wa.edu.au>
2026-08-30 15:11:55 +0800

    initial commit

---
+++ js/task1.js
@@ -0,0 +1,26 @@
+// Version 2.0
+// Portoflio Assignment Task 1
+/*Create a special calculator that read a number from the user, checks that it is an integer and performs a series of mathematical calculations as listed:
+
+Calculate the square and cube of the number. A number squared is a number that is multiplied by itself; e.g., 22 is equal to 4 (2 * 2). A number cubed is a number that is multiplied by itself twice e.g., 23 is equal to 8 (2 * 2 * 2). 
+
+calculates the number's factorial. A factorial is the product of an integer and all the integers below it; e.g., the factorial of 4 is equal to 24 (4 * 3 * 2 * 1).
+*/
+
+function calculate(){
+
+
+
+	return false; 
+}
+// End of calculate() function.
+
+
+function init(){
+
+    
+}
+  // End of init() function.
+
+
+window.onload = init;
